8d2e975919051344422f6698b6a8449ec168c6f5
Gaoyuan Zhang <zgy25483387@gmail.com>
2023-05-04 12:27:29 -0700

    client and server setup

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { React, useState, useEffect } from 'react';
+import { ReactDom, render } from 'react-dom';
+
+
+
+const App = () => {
+    const { productId, setProductId } = useState('');
+    const { outfitArr, setOutfitArr } = useState([]);
+
+    return(
+        <div>Pinapple</div>
+    )
+
+};
+
+export default App;
---
+++ client/src/index.jsx
@@ -0,0 +1,6 @@
+import App from './App.jsx';
+import { React } from 'react';
+// import { ReactDOM, render } from 'react-dom';
+import { render } from 'react-dom';
+
+render(<App />, document.getElementById("root"));
