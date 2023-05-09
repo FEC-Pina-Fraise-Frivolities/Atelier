20087b13448b75e30d4a958ea25dfb1965ee4482
Milad Moulayi <m.moulayi123@gmail.com>
2023-05-09 15:22:40 -0700

    Added clickHandler to thumbnails

--- client/src/components/overview/components/GalleryView.jsx
+++ client/src/components/overview/components/GalleryView.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable consistent-return */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useId, useState } from 'react';
 
 function GalleryView({ selectedStyle, setSelectedStyle }) {
   if (!selectedStyle.name) {
@@ -7,13 +9,27 @@ function GalleryView({ selectedStyle, setSelectedStyle }) {
   }
   const photoUrls = selectedStyle.photos.map((photo) => photo.url);
   const thumbnailUrls = selectedStyle.photos.map((photo) => photo.thumbnail_url);
+  const [selectedPhoto, setSelectedPhoto] = useState(photoUrls[0]);
+
+  const handleClick = (e) => {
+    console.log('e.target.value: ', e.target.src);
+    setSelectedPhoto(e.target.src);
+  };
 
   return (
     <div className="galleryView">
-      <img className="mainPhoto" src={photoUrls[0]} alt={selectedStyle.name} />
+      <img className="mainPhoto" src={selectedPhoto} alt={selectedStyle.name} />
       <div className="thumbnails">
 
-        {thumbnailUrls.map((thumbnail) => <img className="thumbnail" src={thumbnail} alt={selectedStyle.name} />)}
+        {thumbnailUrls.map((thumbnail) => (
+          <img
+            className="thumbnail"
+            src={thumbnail}
+            alt={selectedStyle.name}
+            key={useId()}
+            onClick={handleClick}
+          />
+        ))}
 
       </div>
     </div>
