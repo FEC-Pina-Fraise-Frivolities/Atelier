44135e5cc8505c4b62a68f40547863fa972957a5
Milad Moulayi <m.moulayi123@gmail.com>
2023-05-11 11:30:59 -0700

    Added scrolling functionality to thumbnails

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import RatingAndReview from './components/ratings-and-reviews/index';
 import RelatedAndOutfit from './components/related-items-and-outfit/index';
 
 function App() {
-  const [productId, setProductId] = useState(40436);
+  const [productId, setProductId] = useState(40348);
 
   console.log('current id in app', productId);
 
