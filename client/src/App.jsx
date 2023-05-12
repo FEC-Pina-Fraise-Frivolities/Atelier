2101e5512644e4a8fce39161971157ead2cf705f
Ruojia Liu <rul008@ucsd.edu>
2023-05-12 15:39:13 -0700

    finish all basic functions but css waiting for fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { React, useEffect, useState } from 'react';
 import Overview from './components/overview/Overview';
-import Overview from './components/overview/Overview';
 import RatingAndReview from './components/ratings-and-reviews/index';
 import RelatedAndOutfit from './components/related-items-and-outfit/index';
 import QAIndex from './components/qs-and-as/index';
@@ -11,9 +10,9 @@ function App() {
   return (
     <div>
       <Overview productId={productId} setProductId={setProductId} />
-      <div><RelatedAndOutfit productId ={productId} setProductId = {setProductId}/></div>
+      <div><RelatedAndOutfit productId={productId} setProductId={setProductId} /></div>
       <div><QAIndex /></div>
-      <div><RatingAndReview productId={productId}/></div>
+      <div><RatingAndReview productId={productId} /></div>
     </div>
   );
 }
