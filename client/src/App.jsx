4162db620d69bdc9a77b3aae608c73c7a22938ff
Ruojia Liu <rul008@ucsd.edu>
2023-05-12 14:33:21 -0700

    finish all the basic functions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,14 @@ import RatingAndReview from './components/ratings-and-reviews/index';
 import RelatedAndOutfit from './components/related-items-and-outfit/index';
 
 function App() {
-  const [productId, setProductId] = useState(40438);
+  const [productId, setProductId] = useState(40434);
 
   console.log('current id in app', productId);
 
   return (
     <>
       <RelatedAndOutfit productId={productId} setProductId={setProductId} />
-      <RatingAndReview productId={productId} />
+      {/* <RatingAndReview productId={productId} /> */}
     </>
   );
 }
