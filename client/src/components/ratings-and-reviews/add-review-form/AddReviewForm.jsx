4987954c4b1799b81cd322b2824bea73328f02f4
Gaoyuan Zhang <zgy25483387@gmail.com>
2023-05-13 11:09:46 -0700

    form validation complete

--- client/src/components/ratings-and-reviews/add-review-form/AddReviewForm.jsx
+++ client/src/components/ratings-and-reviews/add-review-form/AddReviewForm.jsx
@@ -47,22 +47,65 @@ function AddReviewForm({
     }
   }
 
-  function submitHandler() {
-    modalHandler();
-    axios.post('/reviews', reviewObj)
-      .then((result) => {
-        console.log(result.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  function dataValidation() {
+    let submit = true;
+    // validate rating, recommend, name
+    const commonValidation = ['rating', 'recommend', 'name'];
+    commonValidation.forEach((key) => {
+      if (reviewObj[key] === '') {
+        submit = false;
+        $(`.${key} .error`).toggle();
+      }
+    });
+
+    // validate body
+    if (reviewObj.body.length < 50) {
+      submit = false;
+      $('.body .error').toggle();
+    }
+
+    // validate email
+    const regex = /@\w*\./g;
+    if (reviewObj.email.length === 0 || reviewObj.email.search(regex) === -1) {
+      submit = false;
+      $('.email .error').toggle();
+    }
+
+    // validate characteristics
+    Object.keys(characteristics).forEach((key) => {
+      const { id } = characteristics[key];
+      if (reviewObj.characteristics[id] === undefined) {
+        submit = false;
+        $(`.${key} .error`).toggle();
+      }
+    });
+    return submit;
   }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    if (dataValidation()) {
+      modalHandler();
+      axios.post('/reviews', reviewObj)
+        .then((result) => {
+          console.log(result.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+
   return (
     <div className="modal">
       <form id="review-form">
         <h1>Write your review</h1>
         <div className="rating selection">
-          <p>Overall Rating</p>
+          <p>
+            Overall Rating
+            <span>&#42;</span>
+            <span className="error"> Please select your rating</span>
+          </p>
           <div className="dynamic-star">
             <span id={5} onClick={starHandler}>&#9733;</span>
             <span id={4} onClick={starHandler}>&#9733;</span>
@@ -72,22 +115,26 @@ function AddReviewForm({
           </div>
         </div>
         <fieldset className="recommend selection">
-          <legend>Do you recommend this product?</legend>
+          <legend>
+            Do you recommend this product?
+            <span>&#42;</span>
+            <span className="error"> Please select your recommendation</span>
+          </legend>
           <div>
             {/* eslint "react/self-closing-comp":0 */}
             <label htmlFor="yes">
               Yes
-              <input type="radio" name="recommend" value onClick={inputHandler}></input>
+              <input type="radio" name="recommend" value required onClick={inputHandler}></input>
             </label>
           </div>
           <div>
             <label htmlFor="no">
               No
-              <input type="radio" name="recommend" value={false} onClick={inputHandler}></input>
+              <input type="radio" name="recommend" value={false} required onClick={inputHandler}></input>
             </label>
           </div>
         </fieldset>
-        <div className="characters selection">
+        <div className="characteristics selection">
           <p>Characteristics Rating</p>
           {Object.keys(characteristics).map((key) => (
             <CharacteristicSelection characteristic={key} key={key} inputHandler={inputHandler} />
@@ -100,13 +147,16 @@ function AddReviewForm({
           </p>
           <textarea name="summary" maxLength={60} placeholder="Best purchase ever!" rows={3} value={reviewObj.summary} onChange={inputHandler}></textarea>
         </div>
-        <div className="details input">
+        <div className="body input">
           <p>
             Review Details
+            <span>&#42;</span>
             <small> (1000 characters max)</small>
+            <span className="error"> Say something about our product (50 characters min). We appreciate and value your feedback!</span>
           </p>
           <textarea
             name="body"
+            type="text"
             maxLength={1000}
             minLength={50}
             placeholder="Why did you like the product or not?"
@@ -114,24 +164,30 @@ function AddReviewForm({
             cols={50}
             value={reviewObj.body}
             onChange={inputHandler}
+            required
           >
           </textarea>
+          <span>{(50 - reviewObj.body.length) > 0 ? `Minimum required characters left: ${50 - reviewObj.body.length}` : 'Minimum reached'}</span>
         </div>
         <PhotoUpload inputHandler={inputHandler} />
-        <div className="nickname input">
+        <div className="name input">
           <p>
             What is your nickname?
+            <span>&#42;</span>
             <small> (60 characters max)</small>
+            <span className="error"> Please enter your name</span>
           </p>
-          <textarea name="name" maxLength={60} placeholder="jackson11" rows={3} value={reviewObj.name} onChange={inputHandler}></textarea>
+          <textarea name="name" maxLength={60} placeholder="jackson11" rows={3} value={reviewObj.name} onChange={inputHandler} required></textarea>
           <small>For privacy reasons, do not use your full name or email address</small>
         </div>
         <div className="email input">
           <p>
-            What is your nickname?
+            What is your email?
+            <span>&#42;</span>
             <small> (60 characters max)</small>
+            <span className="error"> Please enter a valid email</span>
           </p>
-          <textarea name="email" maxLength={60} placeholder="jackson11@email.com" rows={3} value={reviewObj.email} onChange={inputHandler}></textarea>
+          <textarea name="email" maxLength={60} placeholder="jackson11@email.com" rows={3} value={reviewObj.email} onChange={inputHandler} required></textarea>
           <small>For authentication reasons, you will not be emailed</small>
         </div>
         <button type="submit" onClick={submitHandler}>Submit</button>
--- client/src/components/ratings-and-reviews/add-review-form/CharacteristicSelection.jsx
+++ client/src/components/ratings-and-reviews/add-review-form/CharacteristicSelection.jsx
@@ -2,37 +2,41 @@ import React from 'react';
 
 function CharacteristicSelection({ characteristic, inputHandler }) {
   return (
-    <fieldset className="character selection">
-      <legend>{characteristic}</legend>
+    <fieldset className={`${characteristic} selection`}>
+      <legend>
+        {characteristic}
+        <span>&#42;</span>
+        <span className="error"> Please select your rating</span>
+      </legend>
       {/* eslint "react/self-closing-comp":0 */}
       <div>
         <label htmlFor="1">
           Too bad
-          <input type="radio" name={characteristic} value={1} onClick={inputHandler}></input>
+          <input type="radio" name={characteristic} value={1} onClick={inputHandler} required></input>
         </label>
       </div>
       <div>
         <label htmlFor="2">
           Bad
-          <input type="radio" name={characteristic} value={2} onClick={inputHandler}></input>
+          <input type="radio" name={characteristic} value={2} onClick={inputHandler} required></input>
         </label>
       </div>
       <div>
         <label htmlFor="3">
           Okay
-          <input type="radio" name={characteristic} value={3} onClick={inputHandler}></input>
+          <input type="radio" name={characteristic} value={3} onClick={inputHandler} required></input>
         </label>
       </div>
       <div>
         <label htmlFor="4">
           Great
-          <input type="radio" name={characteristic} value={4} onClick={inputHandler}></input>
+          <input type="radio" name={characteristic} value={4} onClick={inputHandler} required></input>
         </label>
       </div>
       <div>
         <label htmlFor="5">
           Love it
-          <input type="radio" name={characteristic} value={5} onClick={inputHandler}></input>
+          <input type="radio" name={characteristic} value={5} onClick={inputHandler} required></input>
         </label>
       </div>
     </fieldset>
