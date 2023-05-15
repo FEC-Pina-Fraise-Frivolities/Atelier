f2853455441f37d2145d2410403e77b109536b9e
Ruojia Liu <rul008@ucsd.edu>
2023-05-15 14:55:34 -0700

    css remain

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,35 +25,7 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
     'react/jsx-no-bind': 'off',
-  },
-};
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'airbnb/hooks',
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-    'no-console': 'off',
-    'react/prop-types': 'off',
-    'import/no-cycle': 'off',
-    'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-noninteractive-element-interactions': 'off',
-    'jsx-a11y/no-static-element-interactions': 'off',
     'consistent-return': 'off',
     'jsx-a11y/label-has-associated-control': 'off',
     'no-alert': 'off',
