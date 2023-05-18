9ca2d142c52ffe4268e20eb8c2dc2c15ad84d3d7
Milad Moulayi <m.moulayi123@gmail.com>
2023-05-18 16:22:07 -0700

    Cleaned up CSS for Features

--- client/src/components/overview/components/Features.jsx
+++ client/src/components/overview/components/Features.jsx
@@ -4,11 +4,12 @@ function Features({ features }) {
   return (
 
     <div className="productFeatures">
-
+      <h4><em>Features</em></h4>
       { features.map((feature) => (
-        <div className="feature">
-          {feature.feature}
+        <div className="feature" key={feature.feature}>
+          <em><b>{feature.feature}</b></em>
           :
+          {'  '}
           {feature.value}
         </div>
       ))}
